a8177967c14064f43ede9cc0cc37a5383132a1c2
Serhii Bohush <jazzist63@gmail.com>
2024-09-10 01:19:57 +0300

    Add posting functionality. Add progress posting page

--- progress/homepage/urls.py
+++ progress/homepage/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	# Create new url pattern
-	path('', views.index, name="index") #views.index means look for a function named index in views.py
+	path('', views.index, name="index"), #views.index means look for a function named index in views.py
+  path('progress', views.progress, name="progress"),
 ]
--- progress/homepage/views.py
+++ progress/homepage/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from datetime import date
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def progress(request):
+    
+    current_date = date.today().strftime('%B %d')
+    if request.method == "POST":
+        progress = request.POST['progress']
+        spent = request.POST['spent']
+        content = {
+            "progress": progress,
+            "spent": spent,
+            "time": current_date
+        }
+        return render(request, "index.html", content)
+    return render(request, "post.html")
