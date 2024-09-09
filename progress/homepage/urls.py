f60a2835ee5a36c593e1d1eafb3de19c9fabaa52
Serhii Bohush <serhii@Macbook-Air-2.local>
2024-09-09 17:49:02 +0300

    Initialize the Django project

---
+++ progress/homepage/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	# Create new url pattern
+	path('', views.index, name="index") #views.index means look for a function named index in views.py
+]
