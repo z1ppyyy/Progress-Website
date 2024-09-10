8a2ddad40103ea98827fde56e55e0f420d96ebe1
Serhii Bohush <jazzist63@gmail.com>
2024-09-10 18:06:12 +0300

    Edit maxlength of progress description

--- progress/homepage/models.py
+++ progress/homepage/models.py
@@ -5,7 +5,7 @@ current_date = date.today().strftime('%B %d')
 
 # Create your models here.
 class Post(models.Model):
-    """Database of posts"""
+    """Table of posts"""
     title = current_date
     progress = models.CharField(max_length=900)
     spent = models.IntegerField(max_length=2)
