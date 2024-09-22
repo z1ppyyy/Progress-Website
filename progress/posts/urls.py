61a7107f1dd86c498048fe4b1533a6c9e39d33db
Serhii Bohush <jazzist63@gmail.com>
2024-09-22 23:58:54 +0300

    Add edit buttons for posts

--- progress/posts/urls.py
+++ progress/posts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name="index"),  # views.index means look for a function named index in views.py
     path('progress', views.progress, name="progress"),
     path('post/<int:id>', views.post_id, name='post_id'),
-    path('delete/<int:id>', views.delete_post, name="delete_post")
+    path('delete/<int:id>', views.delete_post, name="delete_post"),
+    path('edit/<int:id>', views.edit_post, name="edit_post"),
 ]
